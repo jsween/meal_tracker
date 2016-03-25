c437859efeca0547aa24a4b56d40ecaeb4ff18f2
Jon <jonsweeney26@gmail.com>
2016-03-25 08:09:03 -0700

    build meal class

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,14 +3,31 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-    <div class="container center">
-      <h1>Skeleton Angular2 App!</h1>
-      <p>This is also part of the template!</p>
-      <p>Jon said 'always drink upstream from the herd'</p>
+  <div class= "jumbotron center">
+    <h1>Fatboy LoseIt App</h1>
+  </div>
+    <div class="container">
+      <h2>Your Meals</h2>
+      <h4 *ngFor="#meal of meals">{{meal.name}}</h4>
     </div>
   `
 })
 
 export class AppComponent {
+  public meals: Meal[];
+  constructor() {
+    this.meals = [
+      new Meal("Protein Shake", "Chocolate", 160, 0),
+      new Meal("Tiger Thigh", "Endangered Species are the best", 550, 1),
+      new Meal("Small Baby", "Imported from a 3rd world", 680, 2),
+      new Meal("Puppy", "Kind of mangy", 550, 3),
+      new Meal("Eagle", "Missing Hair", 290, 4)
+    ];
+  }
+}
 
+export class Meal {
+  constructor(public name: string, public details: string, public calories: number, public id: number) {
+
+  }
 }
