e305d3603f3a32346b3b8fad4c73b32f3cab2f76
Jon <jonsweeney26@gmail.com>
2016-03-25 09:25:21 -0700

    add new meal entry from

---
+++ app/new-meal.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+import {Meal} from './meal.model';
+
+@Component({
+  selector: 'new-meal',
+  template: `
+    <h4>Blank Template</h4>
+  `
+})
+export class NewMealComponent {
+  
+}
