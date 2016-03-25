e15ed4c98cd23eb2238b37ebaac18fe5d26ec0ab
Jon <jonsweeney26@gmail.com>
2016-03-25 10:11:01 -0700

    clean code

--- app/new-meal.component.ts
+++ app/new-meal.component.ts
@@ -20,7 +20,6 @@ export class NewMealComponent {
   }
   addNewMeal(newName: HTMLInputElement, newDetails: HTMLInputElement, newCalories: HTMLInputElement) {
     var newMeal = new Meal(newName.value, newDetails.value, parseInt(newCalories.value), 0);
-    console.log(newMeal);
     this.onSubmitNewMeal.emit(newMeal);
     newName.value = "";
     newDetails.value = "";
