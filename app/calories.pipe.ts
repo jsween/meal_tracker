6edc24843ff2df2e5c1fd665c62f62d8bddb15da
Jon <jonsweeney26@gmail.com>
2016-03-25 11:12:09 -0700

    add healthy pipe

---
+++ app/calories.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Pipe({
+  name: 'healthyRating',
+  pure: false
+})
+export class HealthyRatingPipe implements PipeTransform {
+  transform(input: Meal[], args) {
+    var desiredHealthyRating = args[0];
+    if (desiredHealthyRating === "unhealthy") {
+      return input.filter((entry) => {
+        return entry.calories > 300;
+      });
+    } else if (desiredHealthyRating === "healthy") {
+      return input.filter((entry) => {
+        return entry.calories <= 300;
+      });
+    } else {
+      return input;
+    }
+  }
+}
